86036bc655486237a3cd91edbd808ca111ab88e0
MuyWild <davidgs.qwerty@gmail.com>
2025-10-27 22:20:16 +0100

    Add additional printf statement for GitHub message

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@ int main() {
     printf("Este proyecto está versionado con Git y GitHub.\n");
 
     printf("¡Mi proyecto C ahora está en GitHub!\n");
+
+     printf("¡Mi proyecto C bajar a VScode desde GitHub!\n");
     
     return 0;
 }

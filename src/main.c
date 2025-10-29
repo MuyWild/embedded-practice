fe9300541728561c1c0bafd6437afe4ae93f50e9
MuyWild <davidgs.qwerty@gmail.com>
2025-10-29 21:04:20 +0100

    Remove commented line in main.c

--- src/main.c
+++ src/main.c
@@ -7,6 +7,8 @@ int main(void) {
     printf("Factorial iterativo de %u = %u\n", n, factorial_iterativo(n));
     printf("Factorial recursivo de %u = %u\n", n, factorial_recursivo(n));
 
+    //bajar actualizacion a PC
+
     return 0;
 }
 
